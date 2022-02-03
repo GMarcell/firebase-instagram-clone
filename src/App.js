f85c49d0cf5f86b79c9ef75b175a3255575e1935
GMarcell <grand1310marcell@gmail.com>
2022-02-03 13:19:37 +0700

    Can Upload To Firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import { useState } from 'react';
 import './App.css';
+import storage from './firebase/config';
 
 function App() {
+  const [progress, setProgress] = useState(0);
+  const formHandler = (e) => {
+    e.preventDefault();
+    const file = e.target[0].files[0];
+    // console.log(file)
+    uploadFile(file)
+  }
+
+  const uploadFile = (file) => {
+    // 
+    if (!file) return;
+    const storageRef = ref(storage, `/files/${file.name}`)
+    const uploadTask = uploadBytesResumable(storageRef, file)
+    uploadTask.on("state_changed", (snapshot) => {
+      const prog = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100)
+      setProgress(prog)
+    }, (err) => console.log(err),
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((url) => console.log(url))
+      })
+  }
+
   return (
     <div className="App">
       <div>
@@ -21,6 +46,12 @@ function App() {
             </div>
           </div>
           <button className='bg-blue-700 w-full rounded text-white font-bold mt-2'>Add Post</button>
+          <form onSubmit={formHandler}>
+            <input type='file' />
+            <button type='submit'>UPLOAD</button>
+          </form>
+          <hr />
+          <h3>Uploaded {progress} % </h3>
         </div>
         <div className='grid grid-flow-row grid-cols-3 gap-4 mt-5'>
           <div className='bg-red-900 rounded'>1</div>
---
+++ src/firebase/config.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDLiC65duYvkp2-DqQHqHREzn5kWb6lPR0",
+    authDomain: "clone-instagram-6f601.firebaseapp.com",
+    projectId: "clone-instagram-6f601",
+    storageBucket: "clone-instagram-6f601.appspot.com",
+    messagingSenderId: "784541952035",
+    appId: "1:784541952035:web:9d604654765519dfb26996",
+    measurementId: "G-2XGCN0KTXS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const storage = getStorage(app);
+
+export default storage;
+
